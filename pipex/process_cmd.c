993bcb93d8369ed7d0c28cf0768632cb8011eb74
Yuen Zheng Chng <ychng@u90z01s02.42kl.edu.my>
2023-11-04 19:05:46 +0800

    process_cmd setup

--- pipex/process_cmd.c
+++ pipex/process_cmd.c
@@ -1,21 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   process_cmd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/28 14:07:18 by ychng             #+#    #+#             */
-/*   Updated: 2023/11/04 19:03:56 by ychng            ###   ########.fr       */
+/*   Created: 2023/11/04 19:04:04 by ychng             #+#    #+#             */
+/*   Updated: 2023/11/04 19:04:38 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "includes/pipex.h"
 
-# include "sys_headers.h"
-# include "libft.h"
-
-void	process_cmd(int argc, char **argv, char **envp);
-
-#endif
+void	process_cmd(int argc, char **argv, char **envp)
+{
+	
+}
