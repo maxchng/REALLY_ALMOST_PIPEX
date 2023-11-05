8094d3b52d36c25fa87f6c131c6a7933772d8bd5
ychng <ychng@student.42kl.edu.my>
2023-11-05 20:57:16 +0800

    add execute function for all cmd. almost finish with mandatory

--- pipex/process_cmd.c
+++ pipex/process_cmd.c
@@ -6,7 +6,7 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 19:04:04 by ychng             #+#    #+#             */
-/*   Updated: 2023/11/04 21:48:39 by ychng            ###   ########.fr       */
+/*   Updated: 2023/11/05 20:52:51 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,18 +98,79 @@ static void execute_first_cmd(int file_fd, int *pipe_fd, char **cmd_tokens)
 	waitpid(pid, NULL, 0);
 }
 
+static void	execute_last_cmd(int file_fd, int *pipe_fd, char **cmd_tokens)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork function didn't work.\n");
+		perror("can't create child process.\n");
+		close(file_fd);
+		exit(-1);
+	}
+	if (pid == 0)
+	{
+		close(pipe_fd[1]);
+		dup2(pipe_fd[0], STDIN_FILENO);
+		dup2(file_fd, STDOUT_FILENO);
+		execve(cmd_tokens[0], cmd_tokens, NULL);
+		exit(0);
+	}
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	waitpid(pid, NULL, 0);
+}
+
+static void	execute_cmd_in_between(int *pipe_fd, char **cmd_tokens)
+{
+	int		prev_pipe_fd[2];
+	pid_t	pid;
+
+	prev_pipe_fd[0] = pipe_fd[0];
+	prev_pipe_fd[1] = pipe_fd[1];
+	setup_pipe_fd(pipe_fd);
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork function didn't work.\n");
+		exit(-1);
+	}
+	if (pid == 0)
+	{
+		close(prev_pipe_fd[1]);
+		dup2(prev_pipe_fd[0], STDIN_FILENO);
+		close(pipe_fd[0]);
+		dup2(pipe_fd[1], STDOUT_FILENO);
+		execve(cmd_tokens[0], cmd_tokens, NULL);
+		exit(0);
+	}
+	close(prev_pipe_fd[0]);
+	close(prev_pipe_fd[1]);
+	waitpid(pid, NULL, 0);
+}
+
 static void	execute_cmd(int cmd_index, int argc, char **argv, char **cmd_tokens)
 {
 	int		pipe_fd[2];
 	int		file_fd;
 
-	setup_pipe_fd(pipe_fd);
 	if (cmd_index == 2)
 	{
+		setup_pipe_fd(pipe_fd);
 		setup_file_fd(argv[1], &file_fd);
 		execute_first_cmd(file_fd, pipe_fd, cmd_tokens);
 		close(file_fd);
 	}
+	else if (cmd_index == (argc - 2))
+	{
+		file_fd = open(argv[cmd_index + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IROTH | S_IRGRP | S_IRGRP);
+		execute_last_cmd(file_fd, pipe_fd, cmd_tokens);
+		close(file_fd);
+	}
+	else
+		execute_cmd_in_between(pipe_fd, cmd_tokens);
 }
 
 void	process_cmd(int argc, char **argv, char **envp)
